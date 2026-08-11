5b8f239c9874d1e1bafc23ab6f5d7f67e735d054
rcontrisha <rcontrisha@gmail.com>
2026-08-11 16:44:43 +0700

    feat: implement counter and function to add data using useState

---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,49 @@
+function ProductForm({ setProducts }) {
+  return (
+    <div className="max-w-fit">
+      <form
+        className="w-full flex flex-col"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(e.target.name.value);
+          console.log(e.target.stock.value);
+
+          setProducts((prevData) => {
+            return [
+              ...prevData,
+              { name: e.target.name.value, stock: e.target.stock.value },
+            ];
+          });
+        }}
+      >
+        <p className="">Input Data Product</p>
+        <div className="py-2">
+          <label htmlFor="name">Input Nama Product</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="border-2 ml-2 pl-2"
+          />
+        </div>
+        <div>
+          <label htmlFor="stock">Input Stock Product</label>
+          <input
+            type="number"
+            name="stock"
+            id="stock"
+            className="border-2 ml-3 pl-2"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-700 px-4 py-2 text-white rounded-xl my-2 w-fit"
+        >
+          Add Data
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ProductForm;
---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,26 @@
+function ProductTable({ products }) {
+  return (
+    <div className="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default">
+      <table className="w-full text-sm text-left rtl:text-right text-body">
+        <thead className="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
+          <tr>
+            <th className="px-6 py-3 font-medium border-r">Product Name</th>
+            <th className="px-6 py-3 font-medium">Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, idx) => {
+            return (
+              <tr key={idx} className="bg-neutral-primary border-b border-default text-center">
+                <td className="px-6 py-4 border-r">{product.name}</td>
+                <td className="px-6 py-4">{product.stock}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ProductTable;
