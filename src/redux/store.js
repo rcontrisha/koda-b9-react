4f997c9645ff3f761ddc05dd98e4f5c0c9e6f92b
rcontrisha <rcontrisha@gmail.com>
2026-08-22 17:12:52 +0700

    feat: implement redux persist to store todo list data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,43 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistCombineReducers,
+  persistReducer,
+} from "redux-persist";
 
 import surveyReducer from "./slices/SurveySlice";
-import todoReducer from "./slices/TodoSlice"
+import todoReducer from "./slices/TodoSlice";
 
-const survey = configureStore({
-  reducer: {
-    surveyState: surveyReducer,
-    todoState: todoReducer
+const storage = {
+  getItem: (key) => {
+    return Promise.resolve(window.localStorage.getItem(key));
   },
+  setItem: (key, value) => {
+    return Promise.resolve(window.localStorage.setItem(key, value));
+  },
+  removeItem: (key) => {
+    return Promise.resolve(window.localStorage.removeItem(key));
+  },
+};
+
+const persistSurveyConfig = {
+  key: "survey",
+  storage,
+  whitelist: ["survey"]
+}
+
+const persistTodoConfig = {
+  key: "todo",
+  storage,
+  whitelist: ["todo"],
+};
+
+const store = configureStore({
+  reducer: persistCombineReducers(persistSurveyConfig, {
+    surveyState: surveyReducer,
+    todoState: persistReducer(persistTodoConfig, todoReducer),
+  }),
 });
 
-export default survey;
+export const persistor = persistStore(store);
+export default store;
